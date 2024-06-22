74b4fffde63439af5e23f4a042672efe836bb058
Antuane69 <antuanealex49@gmail.com>
2024-06-22 00:23:55 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_Proyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 10;
+            //int y = 20;
+            //int z = 30;
+
+            /*
+            //Variables de Numero
+            int x = 10, y = 10, z = 10;
+
+            string textAge = "-23";
+            int age = Convert.ToInt32(textAge);
+            Console.WriteLine(age);
+
+            string textBN = "900000000";
+            long bigNumber = Convert.ToInt64(textBN);
+            Console.WriteLine(bigNumber);
+
+            string textNegative = "-49.5";
+            double negative = Convert.ToDouble(textNegative);
+            Console.WriteLine(negative);
+
+            string textPre = "5000001";
+            float precision = Convert.ToSingle(textPre);
+            Console.WriteLine(precision);
+
+            string textMoney = "18.99";
+            decimal money = Convert.ToDecimal(textMoney);
+            Console.WriteLine(money);
+
+            //Variables de Texto
+            string name = "Emma";
+            char letter = 'a';
+
+            Console.Write("Tu nombre es ");
+            Console.Write(name);
+            Console.WriteLine();
+            Console.WriteLine(letter);
+            Console.ReadLine();
+
+            //Variables Booleanas
+            bool value = false;
+            bool isMale = true;
+
+            //Operadores
+            int age2 = 23;
+            Console.WriteLine(age2);
+            age2++;
+            Console.WriteLine(age2);
+            age2--;
+            Console.WriteLine(age2);
+
+            age2 += 2;
+            Console.WriteLine(age2);
+            age2 -= 15;
+            Console.WriteLine(age2);
+            age2 *= 10;
+            Console.WriteLine(age2);
+
+            string nombre = "Antuane";
+            nombre += " esta programando";
+            Console.WriteLine(nombre);
+
+            char let = 'a';
+            let += 'b';
+            Console.WriteLine(let);
+
+            int i = 0;
+            Console.WriteLine(++i);
+
+
+            //Residuo
+            int firtnum = 10;
+            int secondNum = 2;
+
+            Console.WriteLine(firtnum%secondNum);*/
+
+            //variables de tipo automatico python
+            var age = 23;
+            var bign = 900000L;
+            var money = 6.000;
+            var nombre = "antuane";
+            var lett = 'b';
+
+            Console.ReadLine();
+        }
+    }
+}
